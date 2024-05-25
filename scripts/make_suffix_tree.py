24b2fd6a96d3a23538b122e325cb99956ded45ae
Dashiell Stander <dstander@protonmail.com>
2024-05-25 14:42:26 -0400

    Making the suffix tree
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

---
+++ scripts/make_suffix_tree.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from tokengrams import MemmapIndex
+from argparse import ArgumentParser
+
+
+parser = ArgumentParser()
+parser.add_argument('--data-path', type='str', required=True)
+parser.add_argument('--output-dir', type='str', required=True)
+
+
+if __name__ == '__main__':
+    args, _ = parser.parse_known_args()
+    tokens_path = Path(args.data_path)
+    index_dir = Path(args.output_path)
+    index_dir.mkdir(parents=True, exist_ok=True)
+    index_path = index_dir / 'suffix_tree.idx'
+    print(f'Writing suffix tree to {index_path}')
+    index = MemmapIndex.build(
+        tokens_path,
+        index_path,
+        verbose=True
+    )
