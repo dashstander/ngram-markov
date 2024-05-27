c76c5b9656539053c4c8b1fcebf6126fcd69464a
Dashiell Stander <dstander@protonmail.com>
2024-05-26 20:59:13 -0400

    Need layer param
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- scripts/train_nanogpt.py
+++ scripts/train_nanogpt.py
@@ -41,7 +41,7 @@ always_save_checkpoint = True # if True, always save a checkpoint after each eva
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_entity = 'dstander'
-wandb_log = False # disabled by default
+wandb_log = True # disabled by default
 wandb_project = 'test-tinystories512'
 wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
