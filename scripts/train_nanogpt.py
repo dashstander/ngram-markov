9dc2b1122144caa897fed572ba30a1059f85f533
Dashiell Stander <dstander@protonmail.com>
2024-05-26 20:34:13 -0400

    Need layer param
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- scripts/train_nanogpt.py
+++ scripts/train_nanogpt.py
@@ -120,7 +120,7 @@ def get_batch(split):
     if split == 'train':
         data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
     else:
-        data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+        data = np.memmap(os.path.join(data_dir, 'validation.bin'), dtype=np.uint16, mode='r')
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
